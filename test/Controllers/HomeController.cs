cc37a57373104b923449aeebf88014f5d3b0822e
Laura DiPiazza <laura.dipiazza@rokkan.com>
2013-04-30 17:13:38 -0500

    second commit

--- test/Controllers/HomeController.cs
+++ test/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace test.Controllers
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
             //changing my stuff.
+            //changing again.
             return View();
         }
 
